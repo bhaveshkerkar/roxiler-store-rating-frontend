ad301adedb36e3bf887e82f50853aedd64fd765f
bhaveshkerkar <bhaveshkerkar9@gmail.com>
2026-05-09 22:36:59 +0530

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
-import logo from "../../assets/logo.png";
+import logo from "../../assets/storesphere.png";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className={styles.logo_container}>
               <img src={logo} className={styles.logo} alt="logo" />
               <p className={`${styles.logo_name}`}>
-                Roxiler <br /> Explore
+                Store <br /> Sphere
               </p>
             </div>
           </Link>
@@ -30,10 +30,10 @@ const Footer = () => {
           <div className={styles.footer_list_heading}>GET IN TOUCH</div>
           <ul>
             <li>
-              <IoMail /> bhaveshkerkar9@gmail.com
+              <IoMail /> storesphere9@gmail.com
             </li>
             <li>
-              <IoMail /> devrajpujari003@gmail.com
+              <IoMail /> spherestore003@gmail.com
             </li>
           </ul>
         </div>
@@ -43,24 +43,26 @@ const Footer = () => {
           <div className={styles.footer_list_heading}>Connect With Us</div>
           <ul>
             <li>
-              <a
-                href="https://instagram.com/devrajjj.dev"
-                target="_blank"
+              <Link
+                to="/"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                // target="_blank"
                 rel="noopener noreferrer"
                 className={styles.social_link}
               >
                 Instagram <FaInstagram className={styles.footer_icon} />
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="https://www.facebook.com/people/Devraj-Pujari/pfbid024kZDfiYmDJ9TyNPi2WHpxWAgLe5zjesrr9nmzDcuPdEekbPFf3sHEVUtYdEW7xb8l"
-                target="_blank"
+              <Link
+                to="/"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                // target="_blank"
                 rel="noopener noreferrer"
                 className={styles.social_link}
               >
                 Facebook <FaFacebook className={styles.footer_icon} />
-              </a>
+              </Link>
             </li>
             {/* <li>
               Twitter <FaTwitter className={styles.footer_icon} />
@@ -74,9 +76,7 @@ const Footer = () => {
 
       <hr />
       {/* copy right */}
-      <p className={`${styles.copyRight}`}>
-        © 2026 Roxiler Stores, Inc
-      </p>
+      <p className={`${styles.copyRight}`}>© 2026 Roxiler Stores, Inc</p>
       <div className={styles.footer_icons}>
         {/* <FaInstagram className={styles.footer_icon} /> */}
         {/* <FaFacebook className={styles.footer_icon} />
